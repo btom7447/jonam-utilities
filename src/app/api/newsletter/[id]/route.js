85c85d001fe6dbbfb95778f7b35c86bbd32da4b7
btom7447 <tombenjamin7447@gmail.com>
2025-10-24 23:32:43 +0100

    migration fixes

--- src/app/api/newsletter/[id]/route.js
+++ src/app/api/newsletter/[id]/route.js
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
 import mongoose from "mongoose";
 import connectDB from "@/lib/mongodb";
-import connectDB from "@/lib/mongodb";
 import Newsletter from "@/models/Newsletter";
 
 const isValidObjectId = (id) => mongoose.Types.ObjectId.isValid(id);
